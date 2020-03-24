831a34ddc42d82d9591dbfb81edc1464141fd6f2
gunneraza <gunnera3a@gmail.com>
2020-03-24 16:18:16 +0500

    add team section

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,31 @@ if (module.hot) {
 
 import Parallax from 'parallax-js';
 import Isotope from 'isotope-layout';
+import Swiper from 'swiper';
+
+
+var swiper = new Swiper('.team-slider', {
+  slidesPerView: 6,
+  loop: true,
+  navigation: {
+    nextEl: '.team-button'
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
+    },
+
+    768: {
+      slidesPerView: 3,
+    },
+
+    1200: {
+      slidesPerView: 4,
+    },
+
+    1400: {
+      slidesPerView: 6
+    }
+  }
+});
